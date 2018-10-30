9666243d29abad72041390ae7dbb890aac43efe5
Nate Cox <nate@firelocked.com>
2018-10-30 19:43:17 -0400

    fixed both problems

--- jobappv1/target/scala-2.12/twirl/main/views/html/createUser.template.scala
+++ jobappv1/target/scala-2.12/twirl/main/views/html/createUser.template.scala
@@ -33,7 +33,7 @@ object createUser extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api
 
 Seq[Any](format.raw/*1.35*/("""
 """),_display_(/*3.2*/form(CSRF(routes.ProfileController.createProfile))/*3.52*/{_display_(Seq[Any](format.raw/*3.53*/("""
-
+"""),_display_(/*4.2*/helper/*4.8*/.CSRF.formField),format.raw/*4.23*/("""
 
 """),format.raw/*6.1*/("""<html>
 
@@ -45,23 +45,23 @@ Seq[Any](format.raw/*1.35*/("""
   """),format.raw/*13.3*/("""</style>
   <h1>Create New User</h1>
    <p>
-       First Name :<input type="text" name="firstname" placeholder="First Name" value=""""),_display_(/*16.89*/profileForm("firstname")/*16.113*/.getValue()),format.raw/*16.124*/("""">
-   </p>
+       First Name : """),_display_(/*16.22*/helper/*16.28*/.inputText(field = profileForm("firstName"))),format.raw/*16.72*/("""
+   """),format.raw/*17.4*/("""</p>
    <p>
-       Last Name :<input type="text" name="lastname" placeholder="Last Name" value = """"),_display_(/*19.88*/profileForm("lastname")/*19.111*/.getValue()),format.raw/*19.122*/("""">
-   </p>
+       Last Name :"""),_display_(/*19.20*/helper/*19.26*/.inputText(field = profileForm("lastName"))),format.raw/*19.69*/("""
+   """),format.raw/*20.4*/("""</p>
    <p>
-       Username :<input type="text" name="username" placeholder="Username" value = """"),_display_(/*22.86*/profileForm("username")/*22.109*/.getValue()),format.raw/*22.120*/("""">
-   </p>
+       Username :"""),_display_(/*22.19*/helper/*22.25*/.inputText(field = profileForm("username"))),format.raw/*22.68*/("""
+   """),format.raw/*23.4*/("""</p>
    <p>
-       Password :<input type="password" name="password" placeholder="" value = """"),_display_(/*25.82*/profileForm("password")/*25.105*/.getValue()),format.raw/*25.116*/("""">
-   </p>
+       Password :"""),_display_(/*25.19*/helper/*25.25*/.inputPassword(field = profileForm("password"))),format.raw/*25.72*/("""
+   """),format.raw/*26.4*/("""</p>
    <p>
-       Age :<input type="text" name="age"  value = """"),_display_(/*28.54*/profileForm("age")/*28.72*/.getValue()),format.raw/*28.83*/("""">
-   </p>
+       Age :"""),_display_(/*28.14*/helper/*28.20*/.inputText(field = profileForm("age"))),format.raw/*28.58*/("""
+   """),format.raw/*29.4*/("""</p>
    <p>
-       Biography :<input id="bioForm" type="text" name="biography" placeholder="Describe yourself." value = """"),_display_(/*31.111*/profileForm("biography")/*31.135*/.getValue()),format.raw/*31.146*/("""">
-   </p>
+       Biography :"""),_display_(/*31.20*/helper/*31.26*/.inputText(field = profileForm("biography"))),format.raw/*31.70*/("""
+   """),format.raw/*32.4*/("""</p>
    <p>
      <button type="submit" action=""""),_display_(/*34.37*/routes/*34.43*/.ProfileController.login()),format.raw/*34.69*/("""">Create User</button>
    </p>
@@ -83,11 +83,11 @@ Seq[Any](format.raw/*1.35*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Tue Oct 30 19:15:07 EDT 2018
+                  DATE: Tue Oct 30 19:42:18 EDT 2018
                   SOURCE: C:/sbt/jobAppv1/jobappv1/app/views/createUser.scala.html
-                  HASH: 5dc682c9417d51b48afc7add7eeeb0b528b80ffc
-                  MATRIX: 964->1|1070->37|1115->34|1143->55|1201->105|1239->106|1271->112|1331->145|1359->146|1394->154|1462->195|1490->196|1521->200|1682->334|1716->358|1749->369|1884->477|1917->500|1950->511|2083->617|2116->640|2149->651|2278->753|2311->776|2344->787|2445->861|2472->879|2504->890|2663->1021|2697->1045|2730->1056|2814->1113|2829->1119|2876->1145|2952->1191
-                  LINES: 28->1|31->2|34->1|35->3|35->3|35->3|38->6|41->9|41->9|42->10|44->12|44->12|45->13|48->16|48->16|48->16|51->19|51->19|51->19|54->22|54->22|54->22|57->25|57->25|57->25|60->28|60->28|60->28|63->31|63->31|63->31|66->34|66->34|66->34|69->37
+                  HASH: ebe949aac0ba351f67aa68c10733d6c4a275d68e
+                  MATRIX: 964->1|1070->37|1115->34|1143->55|1201->105|1239->106|1267->109|1280->115|1315->130|1345->134|1405->167|1433->168|1468->176|1536->217|1564->218|1595->222|1689->289|1704->295|1769->339|1801->344|1861->377|1876->383|1940->426|1972->431|2031->463|2046->469|2110->512|2142->517|2201->549|2216->555|2284->602|2316->607|2370->634|2385->640|2444->678|2476->683|2536->716|2551->722|2616->766|2648->771|2725->821|2740->827|2787->853|2863->899
+                  LINES: 28->1|31->2|34->1|35->3|35->3|35->3|36->4|36->4|36->4|38->6|41->9|41->9|42->10|44->12|44->12|45->13|48->16|48->16|48->16|49->17|51->19|51->19|51->19|52->20|54->22|54->22|54->22|55->23|57->25|57->25|57->25|58->26|60->28|60->28|60->28|61->29|63->31|63->31|63->31|64->32|66->34|66->34|66->34|69->37
                   -- GENERATED --
               */
           
