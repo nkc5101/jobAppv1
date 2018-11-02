651590ace5e2aeda6a027f52a0aa0231e607244e
Nate Cox <nate@firelocked.com>
2018-11-02 18:24:47 -0400

    fully added

---
+++ jobappv1/app/controllers/JobData.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.data.validation.Constraints;
+
+public class JobData{
+  @Constraints.Required
+  private String title;
+
+  @Constraints.Required
+  private String description;
+
+  @Constraints.Min(0)
+  private double salary;
+
+  public void setTitle(String title){
+    this.title = title;
+  }
+
+  public void setDescription(String description){
+    this.description = description;
+  }
+
+  public void setSalary(double salary){
+    this.salary = salary;
+  }
+
+  public double getSalary(){
+    return salary;
+  }
+
+  public String getDescription(){
+   return description;
+ }
+
+ public String getTitle(){
+   return title;
+ }
+}
